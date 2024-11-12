8f3b2930a3feef6f7e38feccdecab4b05e99a70f
jaehunLee-git <joz526@naver.com>
2024-11-12 16:09:42 +0900

    Refactor : Swagger 설정 추가

--- src/main/java/com/promptoven/purchaseservice/global/config/SwaggerConfig.java
+++ src/main/java/com/promptoven/purchaseservice/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.promptoven.purchaseservice.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +16,29 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        // Security Secheme명
+        String jwtSchemeName = "jwtAuth";
+
+        // API
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        // SecuritySchemes 설정
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER) //
+                        .name("Authorization"));
+
         return new OpenAPI()
                 .info(new Info()
                         .title("API Test")
-                        .description("product client docs")
+                        .description("cart service docs")
                         .version("1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(components)
                 .servers(List.of(new Server().url("/")));
     }
 }
